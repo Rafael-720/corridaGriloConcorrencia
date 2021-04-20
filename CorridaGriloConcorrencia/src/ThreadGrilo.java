f4ae810fdb011958e135e626aee9ad52a7b1250e
Rafael Oliveira <faelmknv@gmail.com>
2021-04-19 23:13:16 -0300

    Criada a classe que extends Thread
    classe para utilizar threads no software

---
+++ CorridaGriloConcorrencia/src/ThreadGrilo.java
@@ -0,0 +1,88 @@
+
+/**
+ *
+ * @author FAEL
+ */
+public class ThreadGrilo extends Thread{
+    private Grilo grilo;
+    private int linhaChegada;
+    private int totalPercorrido = 0;
+    private int pulos = 0;
+    private static int colocacao = 0;
+    
+
+    public ThreadGrilo(Grilo g, int linhaChegada) {
+        this.setGrilo(g);
+        this.setLinhaChegada(linhaChegada);
+        //this.linhaChegada = linhaChegada;
+        
+    }
+    
+    
+    @Override
+    public void run(){
+        
+        //System.out.println("Thread " + this.getName() + " iniciada com " + this.grilo.getNome());
+        
+        if(totalPercorrido < linhaChegada){
+            
+            
+            this.grilo.pular();
+            
+            this.grilo.setPulos(pulos++);
+            //Time.pulosTime += pulos;
+            
+            this.grilo.setTotalPercorrido(totalPercorrido += this.grilo.getDistanciaRandomica());
+            
+            
+            
+            if(this.totalPercorrido >= this.linhaChegada){                
+                System.out.println("O " + this.grilo.getNome() + " pulou " + this.grilo.getDistanciaRandomica() +
+                    "cm e cruzou a linha de chegada percorrendo " + this.totalPercorrido + "cm");
+                //System.out.println("time do grilo" + this.grilo.getTimeGrilo() + this.grilo.getNome());
+            }else{
+                System.out.println("O " + this.grilo.getNome() + " pulou " + this.grilo.getDistanciaRandomica() +
+                    "cm e já percorreu " + this.totalPercorrido + "cm");
+                //System.out.println("time " + this.grilo.getTimeGrilo() + this.grilo.getNome());
+            }
+                    
+            this.run();
+        }else{
+            colocacao++;
+            System.out.println(this.grilo.getNome() + " foi o " + colocacao + "° e alcançou a linha de chegada com " + pulos + " pulos");
+            //System.out.println("time do grilo" + this.grilo.getTimeGrilo() + this.grilo.getNome());
+        }
+        
+        //System.out.println("Pulos do time " + Time.pulosTime);
+        
+        
+        
+        
+    }
+
+   public void pulosTime(){
+       int numero = this.grilo.getTimeGrilo();
+       
+   }
+
+    
+    public Grilo getGrilo() {
+        return grilo;
+    }
+
+    public void setGrilo(Grilo grilo) {
+        this.grilo = grilo;
+    }
+
+    public int getLinhaChegada() {
+        return linhaChegada;
+    }
+
+    public void setLinhaChegada(int linhaChegada) {
+        this.linhaChegada = linhaChegada;
+    }
+
+    
+    
+    
+}
